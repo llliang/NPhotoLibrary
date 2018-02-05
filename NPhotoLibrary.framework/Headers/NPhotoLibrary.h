7247c4a939e0c4d7053c299eefff930484ffb9b3
Jiang Liang <bluedream5161@gmail.com>
2018-02-05 15:04:44 +0800

    first commit

---
+++ NPhotoLibrary.framework/Headers/NPhotoLibrary.h
@@ -0,0 +1,19 @@
+//
+//  NPhotoLibrary.h
+//  NPhotoLibrary
+//
+//  Created by jiangliang on 05/02/2018.
+//  Copyright © 2018 Jiang Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NPhotoLibrary.
+FOUNDATION_EXPORT double NPhotoLibraryVersionNumber;
+
+//! Project version string for NPhotoLibrary.
+FOUNDATION_EXPORT const unsigned char NPhotoLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NPhotoLibrary/PublicHeader.h>
+
+
